8abc48ca0736e10f3813c6881aa3e36ee6c9e261
revol-xut <revol-xut@protonmail.com>
2021-11-24 19:18:33 +0100

    minimal working version

--- src/shared_header.hpp
+++ src/shared_header.hpp
@@ -7,25 +7,16 @@
 #include <thread>
 #include <fstream>
 #include <filesystem>
-#include <boost/asio.hpp>
 
 struct Event {
     std::string message_;
     unsigned int time_stamp_;
 };
 
-// very simple struct to handle http request
-struct ParsedHeader {
-    std::string method_;
-    std::string endpoint_;
-    std::string body_;
-};
-
 constexpr const char* kMusicDir = "/home/revol-xut/music/AlarmClock/";
 constexpr const char* kFile = "./alarm_clock_events.csv";
 
-constexpr short kBackLog = 30;
-constexpr unsigned short kPort = 6666;
+constexpr unsigned short kPort = 8680;
 
 #endif //SHARED_HEADER_INCLUDE_GUARD
 
