99822fb6336970fd61ef8dee368f6d091ee9532b
revol-xut <revol-xut@protonmail.com>
2021-11-25 21:20:10 +0100

    problems with translation unit sizes

--- src/AlarmClock.cmake
+++ src/AlarmClock.cmake
@@ -1,2 +1,7 @@
 find_package (Threads)
 target_link_libraries(${LF_MAIN_TARGET} ${CMAKE_THREAD_LIBS_INIT} )
+
+set(CROW_INCLUDE /home/revol-xut/workspace/Crow/include/)
+
+target_include_directories(${LF_MAIN_TARGET} PUBLIC /home/revol-xut/workspace/Crow/include/)
+#target_include_directories(${LF_MAIN_TARGET} "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_INCLUDEDIR}")
