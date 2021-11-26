602e191d00e5c88f3ed65bcfeb018ed4d50fd8b5
revol-xut <revol-xut@protonmail.com>
2021-11-26 18:12:39 +0100

    added crow as submodule

--- src/AlarmClock.cmake
+++ src/AlarmClock.cmake
@@ -1,7 +1,8 @@
 find_package (Threads)
-target_link_libraries(${LF_MAIN_TARGET} ${CMAKE_THREAD_LIBS_INIT} )
 
-set(CROW_INCLUDE /home/revol-xut/workspace/Crow/include/)
+set(CROW_HEADERS ../../Crow/include/)
+set(CPP_SOURCES ../../src/)
 
-target_include_directories(${LF_MAIN_TARGET} PUBLIC /home/revol-xut/workspace/Crow/include/)
-#target_include_directories(${LF_MAIN_TARGET} "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_INCLUDEDIR}")
+target_link_libraries(${LF_MAIN_TARGET} ${CMAKE_THREAD_LIBS_INIT})
+target_include_directories(${LF_MAIN_TARGET} PUBLIC ${CROW_HEADERS} ${CPP_SOURCES})
+
