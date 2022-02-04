4b9dc0241bde2fd82a44fd39e88261f44d242dd6
revol-xut <revol-xut@protonmail.com>
2022-02-04 18:55:09 +0100

    src/shared_header.hpp: command as constexpr var

--- src/shared_header.hpp
+++ src/shared_header.hpp
@@ -24,6 +24,7 @@ struct Event {
 
 constexpr const char* kMusicDir = "~/music/AlarmClock/";
 constexpr const char* kFile = "./alarm_clock_events.csv";
+constexpr const char* kPlayerCommand = "mpg321";
 
 constexpr unsigned short kPort = 8680;
 
